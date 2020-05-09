1e3b504d7e378a368f0d887d42454bf39fd56e43
SergioF <fs220381@gmail.com>
2020-05-09 20:32:36 +0300

    Добавьте файлы проекта.

---
+++ solved-leetcode-cases/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solved_leetcode_cases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
