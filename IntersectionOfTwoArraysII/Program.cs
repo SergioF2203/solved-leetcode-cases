4866fa38e9b81ca83371dfca0b5522acb51c32df
SergioF <fs220381@gmail.com>
2020-05-11 20:09:01 +0300

    case result. aproach with dictionary Tvalue = count of a value in an array.

--- IntersectionOfTwoArraysII/Program.cs
+++ IntersectionOfTwoArraysII/Program.cs
@@ -11,6 +11,9 @@ namespace IntersectionOfTwoArraysII
         // Given two arrays, write a function to compute their intersection.
         static void Main(string[] args)
         {
+            var nums1 = new int[] {1, 2, 2, 1 };
+            var nums2 = new int[] { 2, 2 };
+            Solution.Intersect(nums1, nums2);
         }
     }
 }
--- IntersectionOfTwoArraysII/Solution.cs
+++ IntersectionOfTwoArraysII/Solution.cs
@@ -10,7 +10,34 @@ namespace IntersectionOfTwoArraysII
     {
         public static int[] Intersect(int[] nums1, int[] nums2)
         {
+            var numsDic = new Dictionary<int, int>();
+            var resultList = new List<int>();
 
+            foreach(var item in nums1)
+            {
+                if (!numsDic.ContainsKey(item))
+                    numsDic.Add(item, 1);
+                else numsDic[item]++;
+            }
+
+            foreach(var item in nums2)
+            {
+                if (numsDic.ContainsKey(item))
+                {
+                    if(numsDic[item] > 1)
+                    {
+                        numsDic[item]--;
+                        resultList.Add(item);
+                    }
+                    else
+                    {
+                        resultList.Add(item);
+                        numsDic.Remove(item);
+                    }
+                }
+            }
+
+            return resultList.ToArray();
         }
     }
 }
