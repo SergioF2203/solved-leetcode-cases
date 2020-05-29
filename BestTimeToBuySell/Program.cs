db5e3b578e5effc5b0f326c45feb8a3a1f3b8910
SergioF <fs220381@gmail.com>
2020-05-29 12:45:11 +0300

    + Best Time To Sell case

---
+++ BestTimeToBuySell/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuySell
+{
+    class Program
+    {
+        //Say you have an array for which the ith element is the price of a given stock on day i.
+        // If you were only permitted to complete at most one transaction(i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+        // Note that you cannot sell a stock before you buy one.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BestTimeToBuySell/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuySell
+{
+    class Solution
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            return 0;
+        }
+    }
+}
