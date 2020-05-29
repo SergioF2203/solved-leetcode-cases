2a5977071ae4a6a28bb3b2a9f17f4dac788e041b
SergioF <fs220381@gmail.com>
2020-05-29 12:50:54 +0300

    solved Best Time To Buy Sell II

--- BestTimeToBuySellII/Solution.cs
+++ BestTimeToBuySellII/Solution.cs
@@ -10,7 +10,36 @@ namespace BestTimeToBuySellII
     {
         public static int MaxProfit2(int[] prices)
         {
-            return 0;
+            var maxProfit = 0;
+
+            if (prices.Length == 0) return maxProfit;
+
+            var minBuy = prices[0];
+
+            var stackProfit = new Stack<int>();
+            stackProfit.Push(0);
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < minBuy) minBuy = prices[i];
+                var res = prices[i] - minBuy;
+
+                if (stackProfit.Peek() <= res)
+                {
+                    stackProfit.Pop();
+                    stackProfit.Push(res);
+                }
+                else
+                {
+                    stackProfit.Push(0);
+                    minBuy = prices[i];
+                }
+            }
+
+            while (stackProfit.Count > 0)
+                maxProfit += stackProfit.Pop();
+
+            return maxProfit;
         }
     }
 }
