25fa1f5f554c3b8b6a8f9c31dba94028a9434c1a
SergioF <fs220381@gmail.com>
2020-05-29 12:50:19 +0300

    + Best Time To Buy Sell II case

---
+++ BestTimeToBuySellII/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuySellII
+{
+    class Program
+    {
+        // Say you have an array prices for which the ith element is the price of a given stock on day i.
+        // Design an algorithm to find the maximum profit.You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+        // Note: You may not engage in multiple transactions at the same time(i.e., you must sell the stock before you buy again).
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ BestTimeToBuySellII/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuySellII
+{
+    class Solution
+    {
+        public static int MaxProfit2(int[] prices)
+        {
+            return 0;
+        }
+    }
+}
