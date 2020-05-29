6156465d0d5f01a974f19d74b0c7183e94011dc4
SergioF <fs220381@gmail.com>
2020-05-29 12:45:55 +0300

    solved Best Time To Buy Sell

--- BestTimeToBuySell/Solution.cs
+++ BestTimeToBuySell/Solution.cs
@@ -10,7 +10,18 @@ namespace BestTimeToBuySell
     {
         public static int MaxProfit(int[] prices)
         {
-            return 0;
+            if (prices.Length == 0) return 0;
+
+            int res = 0;
+            int min = prices[0];
+
+            for (int i = 1; i < prices.Length - 1; i++)
+            {
+                if (prices[i] < min) min = prices[i];
+                res = prices[i + 1] - min > res ? prices[i + 1] - min : res;
+            }
+
+            return res;
         }
     }
 }
