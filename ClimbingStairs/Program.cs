f1d2002ca19433c50f8d6ab3bd3a283c52db6dc1
SergioF <fs220381@gmail.com>
2020-05-31 11:55:10 +0300

    + Climbing Stairs case

---
+++ ClimbingStairs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimbingStairs
+{
+    class Program
+    {
+        // You are climbing a stair case. It takes n steps to reach to the top.
+        // Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top?
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ClimbingStairs/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimbingStairs
+{
+    class Solution
+    {
+        public static int ClimbStairs(int n)
+        {
+            return 0;
+        }
+    }
+}
