360a14b33e3f6bcc8193848fb16ce2082437e439
SergioF <fs220381@gmail.com>
2020-05-31 11:52:11 +0300

    solved Bulls And Cows case

--- BullsAndCows/Solution.cs
+++ BullsAndCows/Solution.cs
@@ -10,7 +10,36 @@ namespace BullsAndCows
     {
         public static string GetHint(string secret, string guess)
         {
-            return string.Empty;
+            var bulls = 0;
+            var cows = 0;
+
+            bool[] used = new bool[secret.Length];
+            int[] frequency = new int[10];
+
+            for (int i = 0; i < secret.Length; ++i)
+            {
+                if (secret[i] == guess[i])
+                {
+                    used[i] = true;
+                    ++bulls;
+                }
+                else
+                {
+                    frequency[secret[i] - '0'] += 1;
+                }
+            }
+
+            for (int i = 0; i < guess.Length; ++i)
+            {
+                if (!used[i] && frequency[guess[i] - '0'] > 0)
+                {
+                    ++cows;
+                    frequency[guess[i] - '0'] -= 1;
+                }
+            }
+
+
+            return $"{bulls}A{cows}B";
         }
 
     }
