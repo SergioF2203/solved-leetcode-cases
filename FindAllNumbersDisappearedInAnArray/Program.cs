1499dec6ee1c3d89525d6d7f10719ef09a133e67
SergeyF <fs220381@gmail.com>
2021-01-31 20:11:29 +0200

    solved Find Alll Numbers Disappear in an Array case

---
+++ FindAllNumbersDisappearedInAnArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindAllNumbersDisappearedInAnArray
+{
+    class Program
+    {
+        // Given an array of integers where 1 ≤ a[i] ≤ n(n = size of array), some elements appear twice and others appear once.
+        // Find all the elements of [1, n] inclusive that do not appear in this array.
+        static void Main(string[] args)
+        {
+            var res = Solution.FindDisappearedNumbers(new int[] { 4, 3, 3, 2, 2, 1, 7, 8 });
+        }
+    }
+}
---
+++ FindAllNumbersDisappearedInAnArray/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAllNumbersDisappearedInAnArray
+{
+    static class Solution
+    {
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] > 0) 
+                    nums[index] = -nums[index];
+            }
+
+            List<int> result = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0) 
+                    result.Add(i + 1);
+            }
+
+            return result;
+        }
+    }
+}
